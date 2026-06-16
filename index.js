28c9f823b944fa5bb95b546767122e61613c0447
thhatap <125211066+thhatap@users.noreply.github.com>
2026-06-17 03:04:11 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -145,6 +145,4 @@ bot.catch((err, ctx) => {
 
 bot.launch();
 
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
 bot.launch().then(() => console.log("Bot đã sẵn sàng bú tin nhắn!"));
